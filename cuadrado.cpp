ede7105935939891daf6f8bbf1509e02018eb092
Python master <55576353+brinquisruben@users.noreply.github.com>
2019-12-03 12:04:10 +0100

    Add files via upload

---
+++ cuadrado.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+int main(){
+    //definicion de variables
+    char salir;
+    int lado;
+    int col,fil;
+    std::cout <<"Dime el tamano del cuadrado: ";
+    std::cin >>lado;
+    for(col=1;col<=lado;col++){
+    for(fil=1;fil<=lado;fil++){
+    std::cout<<'*';
+    std::cout<<' ';
+                                                          } 
+std::cout<<'\n';
+}                            
+
+std::cin>>salir;
+}
